1c41daa63036a4094062b0e5a9b6b6bfe811e0df
Jihun Kim <rlawlgns3433@gmail.com>
2022-02-08 19:57:37 +0900

    commit

--- Project2/bj1072.cpp
+++ Project2/bj1072.cpp
@@ -19,27 +19,27 @@
 //    }
 //    std::cout << count;
 //}
-
-#include <iostream>
-#define MAX 1000000000
-int main() {
-	long long x, y; int z;
-	std::cin >> x >> y;
-	z = 100 * y / x;
-	int mid, per, right = MAX, left = 0;
-	if (z >= 99) { std::cout << -1; return 0; }
-	int count = -1;
-	while (left <= right) {
-		mid = (right + left) / 2;
-		per = 100 * (y + mid) / (x + mid);
-		if (per > z)
-			right = mid - 1;
-		else {
-			left = mid + 1;
-			count = mid + 1;
-		}
-	}
-	std::cout << count;
-
-	
-}
+//
+//#include <iostream>
+//#define MAX 1000000000
+//int main() {
+//	long long x, y; int z;
+//	std::cin >> x >> y;
+//	z = 100 * y / x;
+//	int mid, per, right = MAX, left = 0;
+//	if (z >= 99) { std::cout << -1; return 0; }
+//	int count = -1;
+//	while (left <= right) {
+//		mid = (right + left) / 2;
+//		per = 100 * (y + mid) / (x + mid);
+//		if (per > z)
+//			right = mid - 1;
+//		else {
+//			left = mid + 1;
+//			count = mid + 1;
+//		}
+//	}
+//	std::cout << count;
+//
+//	
+//}
