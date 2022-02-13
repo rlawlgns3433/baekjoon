bfb4e10ce8ad5ebf804d5c62a9271935c9930737
rlawlgns3433 <rlawlgns3433@gmail.com>
2022-02-13 16:54:56 +0900

    2022/02/13 commit

--- Project2/bj1259.cpp
+++ Project2/bj1259.cpp
@@ -4,16 +4,17 @@
 
 int main()
 {
-    char symbol, str[100000];
-    while (scanf("%s", str)) {
-        if (str[0] == '0') break;
-        int size = strlen(str), count = 0;
-        for (int i = 0; i < size; i++) {
-            symbol = str[i];
-            if (symbol == str[size - i - 1]);
-            else count++;
-        }
-        if (count != 0) printf("no\n");
-        else printf("yes\n");
-    }
+   char symbol, str[100000];
+   while (scanf("%s", str)) {
+       if (str[0] == '0') break;
+       int size = strlen(str), count = 0;
+       for (int i = 0; i < size; i++) {
+           symbol = str[i];
+           if (symbol == str[size - i - 1]);
+           else count++;
+       }
+       if (count != 0) printf("no\n");
+       else printf("yes\n");
+   }
+   getchar();
 }
