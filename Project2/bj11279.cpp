c0defd341b9e4b203d61053f7a1bffc0b0da03e6
Jihun Kim <rlawlgns3433@gmail.com>
2022-02-21 00:14:17 +0900

    2022/02/21 commit

--- Project2/bj11279.cpp
+++ Project2/bj11279.cpp
@@ -1,24 +1,24 @@
-#include <iostream> 
-#include <queue>
-using namespace std;
-
-int main()
-{
-	cin.tie(0);
-	cin.sync_with_stdio(0);
-	int n;
-	cin >> n;
-	priority_queue<int> heap;
-	for(int i = 0; i < n; i++){
-		int  num;
-		cin >> num;
-		if (num == 0) {
-			if (heap.size() == 0) cout << 0 << '\n';
-			else {
-				cout << heap.top() << '\n';
-				heap.pop();
-			}
-		}
-		else heap.push(num);
-	}
-}
+//#include <iostream> 
+//#include <queue>
+//using namespace std;
+//
+//int main()
+//{
+//	cin.tie(0);
+//	cin.sync_with_stdio(0);
+//	int n;
+//	cin >> n;
+//	priority_queue<int> heap;
+//	for(int i = 0; i < n; i++){
+//		int  num;
+//		cin >> num;
+//		if (num == 0) {
+//			if (heap.size() == 0) cout << 0 << '\n';
+//			else {
+//				cout << heap.top() << '\n';
+//				heap.pop();
+//			}
+//		}
+//		else heap.push(num);
+//	}
+//}
---
+++ Project2/bj1927.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+	cin.tie(0);
+	cin.sync_with_stdio(0);
+	int n;
+	priority_queue<int, vector<int>,greater<int>> min_heap;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int num;
+		cin >> num;
+		if (num) {
+			min_heap.push(num);
+		}
+		else {
+			if (min_heap.size() == 0) {
+				cout << 0 << '\n';
+			}
+			else {
+				cout << min_heap.top() << '\n';
+				min_heap.pop();
+			}
+		}
+	
+	}
+}
