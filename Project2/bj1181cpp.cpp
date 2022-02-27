0a4b096ad6a97d2786b72a9b2aa53be890978fbb
Jihun Kim <rlawlgns3433@gmail.com>
2022-02-27 12:46:20 +0900

    2022/02/27 commit

--- Project2/bj1181cpp.cpp
+++ Project2/bj1181cpp.cpp
@@ -11,14 +11,6 @@ bool compareword(string& a, string& b) {
 	return a.length() < b.length();
 }
 
-bool find(string s, vector<string> set) {
-	for (int i = 0; i < set.size(); i++) {
-		if (s == set[i]) {
-			return 1;
-		}
-	}
-	return 0;
-}
 
 int main()
 {
@@ -28,47 +20,10 @@ int main()
 	vector<string> str_vec;
 	for(int i = 0; i < N; i++){
 		string temp_str; cin >> temp_str;
-		if (find(temp_str, str_vec)) continue;
-		else str_vec.emplace_back(temp_str);
+		if (find(str_vec.begin(), str_vec.end(), temp_str) == str_vec.end()) str_vec.emplace_back(temp_str);
 	}
 	sort(str_vec.begin(), str_vec.end(),compareword);
 	for (auto& ele : str_vec) {
 		cout << ele << '\n';
 	}
 }
-
-
-
-//
-//#include <string>
-//#include <vector>
-//#include <iostream>
-//using namespace std;
-//
-//int count(int n) {
-//    int count = 0;
-//    for (int i = 1; i <= n; i++)
-//        if (n % i == 0)
-//            count++;
-//    if (count % 2 != 0)
-//        return n * (-1);
-//    return n;
-//}
-//
-//int solution(int left, int right) {
-//    int answer = 0;
-//    for (int i = left; i <= right; i++) {
-//        if (count(i) % 2 == 0)
-//            answer += count(i);
-//        else {
-//            answer -= count(i);
-//        }
-//    }
-//    return answer;
-//}
-//int main()
-//{
-//
-//
-//    cout << solution(13, 17) << endl;
-//}
