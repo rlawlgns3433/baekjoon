d328d9995e75340b3166b0510893b8cf00ded49b
Jihun Kim <rlawlgns3433@gmail.com>
2022-03-01 23:50:22 +0900

    2022/03/01

--- Project2/bj11047.cpp
+++ Project2/bj11047.cpp
@@ -1,23 +1,23 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	cin.tie(0); cin.sync_with_stdio(0);
-	int N, K;
-	cin >> N >> K;
-	int* c = new int[N];
-	for (int i = 1; i <= N; i++) {
-		cin >> c[N-i];
-	}
-
-	int i = 0, count = 0;
-	while (K != 0) {
-		if (K / c[i]) {
-			count += K / c[i];
-			K = K % c[i];
-		}
-		else i++;
-	}
-	cout << count;
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	cin.tie(0); cin.sync_with_stdio(0);
+//	int N, K;
+//	cin >> N >> K;
+//	int* c = new int[N];
+//	for (int i = 1; i <= N; i++) {
+//		cin >> c[N-i];
+//	}
+//
+//	int i = 0, count = 0;
+//	while (K != 0) {
+//		if (K / c[i]) {
+//			count += K / c[i];
+//			K = K % c[i];
+//		}
+//		else i++;
+//	}
+//	cout << count;
+//}
