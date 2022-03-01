e334dcb6197555952bb34943b155b87e89c28f90
Jihun Kim <rlawlgns3433@gmail.com>
2022-03-01 13:53:10 +0900

    2022/03/01 commit

--- Project2/bj1181cpp.cpp
+++ Project2/bj1181cpp.cpp
@@ -1,29 +1,29 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include <vector>
-using namespace std;
-
-bool compareword(string& a, string& b) {
-	if (a.length() == b.length()) {
-		return a < b;
-	}
-	return a.length() < b.length();
-}
-
-
-int main()
-{
-	cin.tie(0); cin.sync_with_stdio(0);
-	int N;
-	cin >> N;
-	vector<string> str_vec;
-	for(int i = 0; i < N; i++){
-		string temp_str; cin >> temp_str;
-		if (find(str_vec.begin(), str_vec.end(), temp_str) == str_vec.end()) str_vec.emplace_back(temp_str);
-	}
-	sort(str_vec.begin(), str_vec.end(),compareword);
-	for (auto& ele : str_vec) {
-		cout << ele << '\n';
-	}
-}
+//#include <iostream>
+//#include <string>
+//#include <algorithm>
+//#include <vector>
+//using namespace std;
+//
+//bool compareword(string& a, string& b) {
+//	if (a.length() == b.length()) {
+//		return a < b;
+//	}
+//	return a.length() < b.length();
+//}
+//
+//
+//int main()
+//{
+//	cin.tie(0); cin.sync_with_stdio(0);
+//	int N;
+//	cin >> N;
+//	vector<string> str_vec;
+//	for(int i = 0; i < N; i++){
+//		string temp_str; cin >> temp_str;
+//		if (find(str_vec.begin(), str_vec.end(), temp_str) == str_vec.end()) str_vec.emplace_back(temp_str);
+//	}
+//	sort(str_vec.begin(), str_vec.end(),compareword);
+//	for (auto& ele : str_vec) {
+//		cout << ele << '\n';
+//	}
+//}
