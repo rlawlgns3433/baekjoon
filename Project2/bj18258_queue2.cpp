6f09c4482e36eda9bebbef0da15c0772f6264a28
Jihun Kim <rlawlgns3433@gmail.com>
2022-03-09 19:09:37 +0900

    2022/03/09 commit

--- Project2/bj18258_queue2.cpp
+++ Project2/bj18258_queue2.cpp
@@ -1,42 +1,42 @@
-#include <iostream>
-#include <queue>
-#include <string>
-using namespace std;
-
-int main()
-{
-	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	queue<int> q;
-	int n; cin >> n;
-	string str;
-	for (int i = 0; i < n; i++) {
-		cin >> str;
-		if (str == "push") {
-			int number;
-			cin >> number;
-			q.emplace(number);
-		}
-		else if (str == "pop") {
-			if (q.empty()) cout << -1 << '\n';
-			else {
-				cout << q.front() << '\n';
-				q.pop();
-			}
-		}
-		else if (str == "size") {
-			cout << q.size() << '\n';
-		}
-		else if (str == "empty") {
-			if (q.empty()) cout << 1 << '\n';
-			else cout << 0 << '\n';
-		}
-		else if (str == "front") {
-			if (q.empty()) cout << -1 << '\n';
-			else cout << q.front() << '\n';
-		}
-		else if (str == "back") {
-			if (q.empty()) cout << -1 << '\n';
-			else cout << q.back() << '\n';
-		}
-	}
-}
+//#include <iostream>
+//#include <queue>
+//#include <string>
+//using namespace std;
+//
+//int main()
+//{
+//	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+//	queue<int> q;
+//	int n; cin >> n;
+//	string str;
+//	for (int i = 0; i < n; i++) {
+//		cin >> str;
+//		if (str == "push") {
+//			int number;
+//			cin >> number;
+//			q.emplace(number);
+//		}
+//		else if (str == "pop") {
+//			if (q.empty()) cout << -1 << '\n';
+//			else {
+//				cout << q.front() << '\n';
+//				q.pop();
+//			}
+//		}
+//		else if (str == "size") {
+//			cout << q.size() << '\n';
+//		}
+//		else if (str == "empty") {
+//			if (q.empty()) cout << 1 << '\n';
+//			else cout << 0 << '\n';
+//		}
+//		else if (str == "front") {
+//			if (q.empty()) cout << -1 << '\n';
+//			else cout << q.front() << '\n';
+//		}
+//		else if (str == "back") {
+//			if (q.empty()) cout << -1 << '\n';
+//			else cout << q.back() << '\n';
+//		}
+//	}
+//}
---
+++ Project2/bj2161_card1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(); cin.tie(0); cout.tie(0);
+    queue<int> q;
+    int n; cin >> n;
+    for (int i = 1; i <= n; i++) {
+        q.push(i);
+    }
+
+    while (!q.empty()) {
+        cout << q.front() << ' ';
+        q.pop();
+        if (q.empty()) { break; }
+        q.push(q.front());
+        q.pop();
+    }
+}
