67e23a881e759cc55b13b0db7aa451df17886bc4
rlawlgns3433 <rlawlgns3433@gmail.com>
2022-05-30 22:56:22 +0900

    듣보잡

--- Project2/_test.cpp
+++ Project2/_test.cpp
@@ -1,1 +1,66 @@
-﻿
+﻿#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+int n, m;
+
+//체스판 다시 칠하기 (브루트포스 문제) 계속 풀기
+int check_chess_board(vector<vector<char>> chess, char first_char,int i1, int j1)
+{
+	int count = 0;
+	for (int i = i1; i <= i1+7; i++)
+	{
+		for (int j = j1;j <= j1+7; j++)
+		{
+			if (first_char == 'W')
+			{
+				if ((i + j) % 2 == 0 && chess[i][j] != 'W')
+					count++;
+				else if ((i + j) % 2 == 1 && chess[i][j] != 'B')
+					count++;
+			}
+			else if (first_char == 'B')
+			{
+				if ((i + j) % 2 == 0 && chess[i][j] != 'B')
+					count++;
+				else if ((i + j) % 2 == 1 && chess[i][j] != 'W')
+					count++;
+			}
+		}
+	}
+		return count;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0);
+	cin >> n >> m;
+	vector<vector<char>> chess;
+	chess.resize(n + 1 , vector<char>(m + 1));
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= m; j++)
+		{
+			cin >> chess[i][j];
+		}
+	}
+	char first_char = chess[1][1];
+	
+	int minimum = INT_MAX;
+	for (int i = 1; i <= n - 7; i++)
+	{
+		for (int j = 1; j <= m - 7; j++)
+		{
+			first_char = chess[i][j];
+			minimum = min(minimum, check_chess_board(chess, first_char, i, j));
+		}
+	}
+	if (minimum == 32)
+	{
+		cout << 12;
+		return 0;
+	}
+	cout << minimum;
+}
--- Project2/bj1764_never_been_seen_or_heard_who_is.cpp
+++ Project2/bj1764_never_been_seen_or_heard_who_is.cpp
@@ -1,57 +1,40 @@
-////#include <iostream>
-////#include <vector>
-////#include <string>
-////#include <unordered_set>
-////#include <algorithm>
-////using namespace std;
-////
-////
-////int main()
-////{
-////	cin.tie(0); cin.sync_with_stdio(0);
-////
-////	vector<string> never_heard, never_seen, never_heard_and_seen;
-////
-////	int n, m;
-////	cin >> n >> m;
-////	for (int i = 0; i < n + m; i++) {
-////		string name;
-////		cin >> name;
-////		if (i < n) never_heard.emplace_back(name);
-////		else never_seen.emplace_back(name);
-////	}
-////
-////	for (int i = 0; i < n; i++) {
-////		string never_heard_name = never_heard[i]; 
-////		for (int j = 0; j < m; j++) {
-////			string never_seen_name = never_seen[j];
-////			if (never_heard_name == never_seen_name)
-////				never_heard_and_seen.emplace_back(never_heard_name);
-////		}
-////	}
-////	sort(never_heard_and_seen.begin(), never_heard_and_seen.end());
-////	int size = never_heard_and_seen.size();
-////	cout << size << '\n';
-////	for (const auto& name : never_heard_and_seen)
-////		cout << name << '\n';
-////}
-////
-//#include <iostream>
-//#include <algorithm>
-//#include <vector>
-//#include <string>
-//using namespace std;
-//
-//int main()
-//{
-//	cin.tie(0); cout.tie(0); cin.sync_with_stdio(0); cout.sync_with_stdio(0);
-//	int n, m; cin >> n >> m;
-//	vector<string> n_heard, n_seen, n_heard_seen;
-//	for (int i = 0; i < n; i++)
-//	{
-//		int temp; cin >> n_heard[i];
-//	}
-//	for (int i = 0; i < m; i++) {
-//		int temp; cin >> n_
-//	}
-//}
+#include <iostream>
+#include <vector>
+#include <set>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+set<string> s1;
+vector<string> v, ans;
+int main()
+{
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		string str;
+		cin >> str;
+		s1.insert(str);
+	}
+
+	for (int i = 0; i < m; i++)
+	{
+		string str;
+		cin >> str;
+		v.push_back(str);
+	}
+	for (int i = 0; i < m; i++) 
+	{
+		if (s1.find(v[i]) != s1.end())
+		{
+			ans.push_back(v[i]);
+		}
+	}
+	cout << ans.size() << '\n';
+	sort(ans.begin(), ans.end());
+	for (auto& e : ans)
+	{
+		cout << e << '\n';
+	}
+}
