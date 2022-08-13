ef526d5e36e4ec6ac07dc389e463f0eb9652e93f
지훈 <80836736+rlawlgns3433@users.noreply.github.com>
2022-08-13 22:43:32 +0900

    가장 긴 증가/감소하는 부분 수열

---
+++ Project2/가장 긴 감소하는 부분 부열.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+vector<int> dp(1001, 1);
+vector<int> arr;
+int n;
+
+int main()
+{
+    int n, LIS = 1;
+    cin >> n;
+    dp.resize(n + 1);
+    arr.resize(n + 1);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        dp[i] = 1;
+    }
+
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (arr[j] > arr[i])
+            {
+                if (dp[j] + 1 > dp[i])
+                {
+                    dp[i] = dp[j] + 1;
+                }
+
+                if (LIS < dp[i])
+                    LIS = dp[i];
+            }
+        }
+    }
+    cout << LIS;
+}
