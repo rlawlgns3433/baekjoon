09a922a91d64f87906bca983fece48add5bbbae5
지훈 <80836736+rlawlgns3433@users.noreply.github.com>
2022-10-01 22:51:37 +0900

    트리순회

--- Project2/트리순회.cpp
+++ Project2/트리순회.cpp
@@ -1,4 +1,6 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <stdio.h>
 using namespace std;
 
 struct node
@@ -49,3 +51,63 @@ int main()
 	inorder('A'); cout << endl;
 	postorder('A'); cout << endl;
 }
+
+//typedef struct node* treePointer;
+//typedef struct node {
+//	int data;
+//	treePointer leftChild, rightChild;
+//}node;
+//
+//treePointer makeRootNode(char data, treePointer leftChild, treePointer rightChild)
+//{
+//	treePointer root = (treePointer)malloc(sizeof(node));
+//	root->data = data;
+//	root->leftChild = leftChild;
+//	root->rightChild = rightChild;
+//
+//	return root;
+//}
+//
+//void inorder(treePointer ptr)
+//{
+//	if (ptr->data) {
+//		printf("%d", ptr->data);
+//		inorder(ptr->leftChild);
+//		inorder(ptr->rightChild);
+//	}
+//}
+//
+//void preorder(treePointer ptr)
+//{
+//	if (ptr->data)
+//	{
+//		printf("%d", ptr->data);
+//		preorder(ptr->leftChild);
+//		preorder(ptr->rightChild);
+//	}
+//}
+//
+//void postorder(treePointer ptr)
+//{
+//	if (ptr->data)
+//	{
+//		postorder(ptr->leftChild);
+//		postorder(ptr->rightChild);
+//		printf("%d", ptr->data);
+//	}
+//}
+//
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	treePointer tree = new node[n];
+//	for (int i = 0; i < n; i++)
+//	{
+//		int d, l, r;
+//		scanf("%d %d %d", &d, &l, &r);
+//		if (l) tree[d].leftChild = makeRootNode(l, NULL, NULL);
+//		if (r) tree[d].rightChild = makeRootNode(r, NULL, NULL);
+//	}
+//	inorder(tree);
+//}
