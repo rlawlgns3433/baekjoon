a45a61b00d8ba75533c3c89b1b29d572d86d4163
지훈 <80836736+rlawlgns3433@users.noreply.github.com>
2022-10-16 17:44:33 +0900

    카드 정렬하

--- Project2/_test2.cpp
+++ Project2/_test2.cpp
@@ -1,7 +1,65 @@
+// 가장 가까운 두 점
+// boj2261번 Divide and Conquer
+
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
 using namespace std;
+
+int n;
+double answer = 2e+8;
+
+struct Point
+{
+	int x, y;
+};
+
+bool compare(Point p1, Point p2)
+{
+	return p1.x < p2.x;
+}
+
+double divide(Point* p, int left_x, int right_x)
+{
+	int midOfIndex = (left_x + right_x) / 2;
+	int midOfXValue = p[midOfIndex].x;
+
+	if (p[left_x].x == p[left_x + 1].x && p[left_x].y == p[left_x + 1].y) return 0;
+	if (right_x - left_x == 1)
+	{
+		return sqrt(pow(p[0].x - p[1].x, 2) + pow(p[0].y - p[1].y, 2));
+	}
+	else if (right_x - left_x == 2)
+	{
+		double temp = sqrt(pow(p[0].x - p[1].x, 2) + pow(p[0].y - p[1].y, 2));
+		answer = min(temp, sqrt(pow(p[0].x - p[2].x, 2) + pow(p[0].y - p[2].y, 2)));
+		answer = min(answer, sqrt(pow(p[2].x - p[1].x, 2) + pow(p[2].y - p[1].y, 2)));
+		return answer;
+	}
+	else
+	{
+		double left_min = divide(p, left_x, midOfIndex);
+		double right_min = divide(p, midOfIndex + 1, right_x);
+		answer = min(left_min, right_min);
+	}
+	return pow(answer, 2);
+}
 
 int main()
 {
-	
+	cin >> n;
+
+	Point* p = new Point[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		int x, y;
+		cin >> x >> y;
+		p[i].x = x; p[i].y = y;
+	}
+
+	sort(p, p + n - 1, compare);
+	cout << divide(p, 0, n - 1);
+
 }
--- Project2/카드 정렬하기.cpp
+++ Project2/카드 정렬하기.cpp
@@ -3,35 +3,42 @@
 #include <vector>
 using namespace std;
 
-priority_queue<int, vector<int>, greater<int>> min_heap;
-int acc = 0;
+priority_queue<int, vector<int>, greater<int>> min_heap; // 최소힙
+int acc = 0; // 누적합을 저장하는 변수
 
 int main()
 {
-    int n;
+    int n; // 카드 더미의 개수
     cin >> n;
-    if (n == 1)
+    if (n == 1) // 더미의 개수가 1개라면 비교할 필요가 없으므로 0번 비교 후 종료
     {
         cout << acc;
         exit(0);
     }
 
     for (int i = 0; i < n; i++)
+        // 각 카드 더미가 포함하는 카드의 개수를 입력받아 최소힙에 저장
+        // 카드 개수가 적은 순서대로 저장 (그리디한 특성)
     {
         int num; cin >> num;
         min_heap.push(num);
     }
 
     while (min_heap.size() > 2)
+        // 비교하지 않은 카드 더미의 수가 2개 보다 많다면
     {
-        int num1 = min_heap.top(); min_heap.pop();
+        // 가장 작은 더미 2개를 골라 비교 횟수를 구함 ( num1 + num2 )
+        int num1 = min_heap.top(); min_heap.pop(); 
         int num2 = min_heap.top(); min_heap.pop();
 
-        min_heap.push(num1 + num2);
-        acc += (num1 + num2);
+        min_heap.push(num1 + num2); // 카드 비교 횟수를 다시 최소힙에 저장
+        acc += (num1 + num2); // 누적합을 갱신
     }
+
+    // 최종적으로 남은 두 개의 카드 더미를 비교
     int num1 = min_heap.top(); min_heap.pop();
     int num2 = min_heap.top();
 
+    // 정답은 마지막 더미의 비교 횟수 + 누적합
     cout << acc + num1 + num2;
 }
